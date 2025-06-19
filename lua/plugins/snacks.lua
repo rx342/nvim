5dd6bef8985f9769c87414513770a9c999fbebf6
rx342 <15484682+rx342@users.noreply.github.com>
2025-06-19 21:40:26 +0200

    fix(snacks): explicitly enable/disable features

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,10 +6,19 @@ return {
     require("snacks").setup({
       styles = {},
       bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
       notifier = { enabled = false },
       quickfile = { enabled = false },
+      scope = { enabled = false },
+      scroll = { enabled = false },
       statuscolumn = { enabled = false },
       words = { enabled = false },
+      picker = { enabled = true },
+      git = { enabled = true },
+      toggle = { enabled = true },
     })
   end,
   keys = {
