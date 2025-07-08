01f5d8969d0de102c1eaf4d12d7128da8987a86e
rx342 <15484682+rx342@users.noreply.github.com>
2025-07-08 19:37:49 +0200

    feat(conform): toggle command

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,10 +4,13 @@ return {
   after = function()
     require("conform").setup({
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 5000,
-        lsp_format = "fallback",
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 5000, lsp_format = "fallback" }
+      end,
       formatters_by_ft = {
         markdown = { "prettierd" },
         lua = { "stylua" },
@@ -37,6 +40,24 @@ return {
         bash = { "shfmt" },
         just = { "just" },
       },
+    })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
     })
   end,
 }
