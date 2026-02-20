8088c11259c86f87bc4860076399e806232c0228
rx342 <15484682+rx342@users.noreply.github.com>
2026-02-20 23:12:24 +0100

    fix(lspconfig): vim.fn.sign_define -> vim.diagnostic.config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -102,7 +102,13 @@ return {
 
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      vim.diagnostic.config({
+        signs = {
+          text = icon,
+          texthl = hl,
+          numhl = "",
+        },
+      })
     end
 
     Snacks.toggle.diagnostics():map("<leader>cd")
